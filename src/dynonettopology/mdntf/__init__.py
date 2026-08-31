f2af4ee4aa6354d9cc31a329549edaef0d37021f
Amani <asorirosa5@gmail.com>
2026-08-31 23:38:58 +0200

    Update __init__.py

--- src/dynonettopology/mdntf/__init__.py
+++ src/dynonettopology/mdntf/__init__.py
@@ -1,1 +1,33 @@
+"""Multiscale Dynamic Network–Topology Framework."""
 
+from .complexes import (
+    SimplicialComplex,
+    clique_complex,
+)
+from .filtration import (
+    FiltrationLevel,
+    NetworkFiltration,
+    create_filtration,
+)
+from .persistence import (
+    PersistenceResult,
+    compute_persistence,
+)
+from .tracker import (
+    FeatureTracker,
+    FeatureTrajectory,
+    TopologicalFeature,
+)
+
+__all__ = [
+    "FeatureTracker",
+    "FeatureTrajectory",
+    "FiltrationLevel",
+    "NetworkFiltration",
+    "PersistenceResult",
+    "SimplicialComplex",
+    "TopologicalFeature",
+    "clique_complex",
+    "compute_persistence",
+    "create_filtration",
+]
