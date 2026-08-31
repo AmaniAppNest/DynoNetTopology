f2017921e9fac2378fbad1792187c219ef7e437e
Amani <asorirosa5@gmail.com>
2026-08-31 23:38:19 +0200

    Update __init__.py

--- src/dynonettopology/networks/__init__.py
+++ src/dynonettopology/networks/__init__.py
@@ -1,1 +1,17 @@
+"""Dynamic network analysis components."""
 
+from .construction import (
+    correlation_adjacency,
+    threshold_adjacency,
+)
+from .dynamics import (
+    DynamicNetwork,
+    NetworkState,
+)
+
+__all__ = [
+    "correlation_adjacency",
+    "threshold_adjacency",
+    "DynamicNetwork",
+    "NetworkState",
+]
